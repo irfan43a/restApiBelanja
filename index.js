478f351b09a3e42a1b61ec73187e89f76855359c
Irfan Abidin <98889949+irfan43a@users.noreply.github.com>
2022-05-15 17:06:32 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
+const createError = require("http-errors");
+const path = require("path");
+const helmet = require("helmet");
+const PORT = process.env.PORT || 5000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(morgan("dev"));
+app.use(helmet());
+
+const mainRoute = require("./src/routers");
+
+app.use("/v1", mainRoute);
+app.use("/img", express.static(path.join(__dirname, "./upload")));
+
+app.all("*", (req, res, next) => {
+  next(new createError.NotFound());
+});
+
+app.use((err, req, res) => {
+  const messError = err.message || "Internal server Error";
+  const statusCode = err.status || 500;
+  res.status(statusCode).json({
+    status: statusCode,
+    message: messError,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server starting on port ${PORT}`);
+});
